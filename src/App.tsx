e3ae1cbfb69e397674a27f561aa08b1ffcc302a1
sumanc99 <sumanssy5000@gmail.com>
2026-08-31 21:54:57 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <div className="text-3xl font-bold underline">
+        <h1>SUMAN CHAT</h1>
+        <Button variant='outline'>Su</Button>
+      </div>
+      
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path"
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
